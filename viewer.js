f084104085a0c8bf1f21ebb68106a1b7dd9a7274
alphaoptique <alphaoptique.versailles@gmail.com>
2026-07-04 20:43:22 +0200

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,26 +1,31 @@
-const urlParams = new URLSearchParams(window.location.search);
-const fileUrl = urlParams.get("file");
-
 pdfjsLib.GlobalWorkerOptions.workerSrc =
   "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.6.82/pdf.worker.min.js";
 
 const canvas = document.getElementById("pdf-canvas");
 const ctx = canvas.getContext("2d");
 
-async function loadPDF(url) {
-  const pdf = await pdfjsLib.getDocument(url).promise;
-  const page = await pdf.getPage(1);
+document.getElementById("file-input").addEventListener("change", async (e) => {
+  const file = e.target.files[0];
 
-  const scale = 2; // HD simple
-  const viewport = page.getViewport({ scale });
+  const fileReader = new FileReader();
 
-  canvas.width = viewport.width;
-  canvas.height = viewport.height;
+  fileReader.onload = async function () {
+    const typedarray = new Uint8Array(this.result);
 
-  await page.render({
-    canvasContext: ctx,
-    viewport: viewport
-  }).promise;
-}
+    const pdf = await pdfjsLib.getDocument({ data: typedarray }).promise;
+    const page = await pdf.getPage(1);
 
-loadPDF(fileUrl);
+    const scale = 2;
+    const viewport = page.getViewport({ scale });
+
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+
+    await page.render({
+      canvasContext: ctx,
+      viewport: viewport
+    }).promise;
+  };
+
+  fileReader.readAsArrayBuffer(file);
+});
